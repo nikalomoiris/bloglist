d8094e983fd614591da92c578a769a0d1364356e
nikalomiris <nikalomiris@gmail.com>
2019-07-11 16:43:36 -0400

    Added middleware

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const cors = require('cors');
 const blogsRouter = require('./controllers/blogs');
+const middleware = require('./utils/middleware');
 const mongoose = require('mongoose');
 
 console.log('Connecting to ', config.MONGODB_URI);
@@ -16,6 +17,9 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(middleware.requestLogger);
 app.use('/api/blogs', blogsRouter);
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 module.exports = app;
