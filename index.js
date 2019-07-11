7faf53b690f7903c0f5faa7cd766aa3406170cf8
nikalomiris <nikalomiris@gmail.com>
2019-07-11 10:39:42 -0400

    Heroku integration

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.post('/api/blogs', (req, res) => {
         });
 });
 
-const PORT = 3003;
+const PORT = process.env.PORT || 3003;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
