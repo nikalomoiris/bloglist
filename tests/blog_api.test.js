1f572820cd76dc1baf4630225b36a2ff57194dc2
nikalomiris <nikalomiris@gmail.com>
2019-07-19 07:23:18 -0400

    Part 4 Blog list tests step 3 solution

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -17,6 +17,26 @@ test('the blogs have a property named "id"', async () => {
     expect(response.body[0].id).toBeDefined();
 });
 
+test('posting a blog increases the number of blogs saved', async () => {
+    const initialBlogs = await api.get('/api/blogs');
+
+    const newBlog = {
+        title: 'My sixth Blog post',
+        author: 'Me and you and him',
+        url: 'www.piiiiiiidd.com',
+        likes: 98
+    };
+
+    await api.post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/);
+
+    const finalBlogs = await api.get('/api/blogs');
+
+    expect(initialBlogs.body.length + 1).toBe(finalBlogs.body.length);
+});
+
 afterAll(() => {
     mongoose.connection.close();
 });
