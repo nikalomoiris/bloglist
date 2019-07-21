d2b7d8d539eefe5a17e83661430741ba4e5d76a9
nikalomiris <nikalomiris@gmail.com>
2019-07-21 08:37:00 -0400

    Modified test after the addition of users in db

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -7,124 +7,12 @@ const helper = require('./test_helper');
 
 const api = supertest(app);
 
-describe('Tests for the blogs api', () => {
-    beforeEach(async () => {
-        await Blog.deleteMany({});
-
-        for (let blog of helper.initialBlogs) {
-            let blogObject = new Blog(blog);
-            await blogObject.save();
-        }
-    });
-
-    test('blogs are returned as json', async () => {
-        await api
-            .get('/api/blogs')
-            .expect(200)
-            .expect('Content-Type', /application\/json/);
-    });
-
-    test('blogs have a property named "id"', async () => {
-        const response = await api.get('/api/blogs');
-
-        expect(response.body[0].id).toBeDefined();
-    });
-
-    test('posting a blog increases the number of blogs saved', async () => {
-        const newBlog = {
-            title: 'My sixth Blog post',
-            author: 'Me and you and him',
-            url: 'www.piiiiiiidd.com',
-            likes: 98
-        };
-
-        await api.post('/api/blogs')
-            .send(newBlog)
-            .expect(200)
-            .expect('Content-Type', /application\/json/);
-
-        const finalBlogs = await helper.blogsInDb();
-
-        expect(helper.initialBlogs.length + 1).toBe(finalBlogs.length);
-    });
-
-    test('posting a blog without likes property defaults to 0 likes', async () => {
-        const newBlog = {
-            title: 'My sixth Blog post',
-            author: 'Me and you and him',
-            url: 'www.piiiiiiidd.com'
-        };
-
-        const postedBlog = await api.post('/api/blogs')
-            .send(newBlog)
-            .expect(200)
-            .expect('Content-Type', /application\/json/);
-        expect(postedBlog.body.likes).toBe(0);
-    });
-
-    test('posting a blog without title property returns 400 Bad Request', async () => {
-        const newBlog = {
-            author: 'Me and you and him',
-            url: 'www.piiiiiiidd.com',
-            likes: 98
-        };
-
-        await api.post('/api/blogs')
-            .send(newBlog)
-            .expect(400);
-    });
-
-    test('posting a blog without url property returns 400 Bad Request', async () => {
-        const newBlog = {
-            title: 'My sixth Blog post',
-            author: 'Me and you and him',
-            likes: 98
-        };
-
-        await api.post('/api/blogs')
-            .send(newBlog)
-            .expect(400);
-    });
-
-    test('deleting a blog returns 204 Not Found', async () => {
-        const blogsBeforeDelete = await helper.blogsInDb();
-        const blogToDelete = blogsBeforeDelete[0];
-        const idToDelete = blogToDelete.id;
-
-        await api.delete(`/api/blogs/${idToDelete}`)
-            .expect(204);
-
-        const blogsAfterDelete = await helper.blogsInDb();
-        expect(blogsBeforeDelete.length).toBe(blogsAfterDelete.length + 1);
-
-        const titles = blogsAfterDelete.map(blog => blog.title);
-        expect(titles).not.toContain(blogToDelete.title);
-    });
-
-    test('updating a blog returns 200 OK', async () => {
-        const blogs = await helper.blogsInDb();
-        const blogToUpdate = blogs[0];
-        const idToUpdate = blogToUpdate.id;
-
-        const updatedBlog = {
-            title: blogToUpdate.title,
-            author: blogToUpdate.author,
-            url: blogToUpdate.url,
-            likes: 500
-        };
-
-        await api.put(`/api/blogs/${idToUpdate}`)
-            .send(updatedBlog)
-            .expect(200);
-    });
-});
-
 describe('Test for users api when there is initially one user at db', () => {
-    // beforeEach(async () => {
-    //     await User.deleteMany({});
-    //     const user = new User({ username: 'root', password: 'sekret' });
-    //     await user.save();
-    // });
+    beforeEach(async () => {
+        await User.deleteMany({});
+        const user = new User({ username: 'root', password: 'sekret' });
+        await user.save();
+    });
 
     test('creation succeds with a fresh username', async () => {
         const usersAtStart = await helper.usersInDb();
@@ -167,6 +55,137 @@ describe('Test for users api when there is initially one user at db', () => {
 
         const usersAtEnd = await helper.usersInDb();
         expect(usersAtEnd.length).toBe(usersAtStart.length);
+    });
+});
+
+
+describe('Tests for the blogs api', () => {
+    beforeEach(async () => {
+        const creator = await helper.usersInDb();
+        const creatorId = creator[0].id;
+        await Blog.deleteMany({});
+
+        for (let blog of helper.initialBlogs) {
+            let blogObject = new Blog(blog);
+            blogObject.userId = creatorId;
+            await blogObject.save();
+        }
+    });
+
+    test('blogs are returned as json', async () => {
+        await api
+            .get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type', /application\/json/);
+    });
+
+    test('blogs have a property named "id"', async () => {
+        const response = await api.get('/api/blogs');
+
+        expect(response.body[0].id).toBeDefined();
+    });
+
+    test('posting a blog increases the number of blogs saved', async () => {
+        const creator = await helper.usersInDb();
+        const creatorId = creator[0].id;
+        const newBlog = {
+            title: 'My sixth Blog post',
+            author: 'Me and you and him',
+            url: 'www.piiiiiiidd.com',
+            likes: 98,
+            userId: creatorId
+        };
+
+        await api.post('/api/blogs')
+            .send(newBlog)
+            .expect(200)
+            .expect('Content-Type', /application\/json/);
+
+        const finalBlogs = await helper.blogsInDb();
+
+        expect(helper.initialBlogs.length + 1).toBe(finalBlogs.length);
+    });
+
+    test('posting a blog without likes property defaults to 0 likes', async () => {
+        const creator = await helper.usersInDb();
+        const creatorId = creator[0].id;
+        const newBlog = {
+            title: 'My sixth Blog post',
+            author: 'Me and you and him',
+            url: 'www.piiiiiiidd.com',
+            userId: creatorId
+        };
+
+        const postedBlog = await api.post('/api/blogs')
+            .send(newBlog)
+            .expect(200)
+            .expect('Content-Type', /application\/json/);
+        expect(postedBlog.body.likes).toBe(0);
+    });
+
+    test('posting a blog without title property returns 400 Bad Request', async () => {
+        const creator = await helper.usersInDb();
+        const creatorId = creator[0].id;
+        const newBlog = {
+            author: 'Me and you and him',
+            url: 'www.piiiiiiidd.com',
+            likes: 98,
+            userId: creatorId
+        };
+
+        await api.post('/api/blogs')
+            .send(newBlog)
+            .expect(400);
+    });
+
+    test('posting a blog without url property returns 400 Bad Request', async () => {
+        const creator = await helper.usersInDb();
+        const creatorId = creator[0].id;
+        const newBlog = {
+            title: 'My sixth Blog post',
+            author: 'Me and you and him',
+            likes: 98,
+            userId: creatorId
+        };
+
+        await api.post('/api/blogs')
+            .send(newBlog)
+            .expect(400);
+    });
+
+    test('deleting a blog returns 204 Not Found', async () => {
+        const blogsBeforeDelete = await helper.blogsInDb();
+        const blogToDelete = blogsBeforeDelete[0];
+        const idToDelete = blogToDelete.id;
+
+        await api.delete(`/api/blogs/${idToDelete}`)
+            .expect(204);
+
+        const blogsAfterDelete = await helper.blogsInDb();
+        expect(blogsBeforeDelete.length).toBe(blogsAfterDelete.length + 1);
+
+        const titles = blogsAfterDelete.map(blog => blog.title);
+        expect(titles).not.toContain(blogToDelete.title);
+    });
+
+    test('updating a blog returns 200 OK', async () => {
+        const creator = await helper.usersInDb();
+        const creatorId = creator[0].id;
+        const blogs = await helper.blogsInDb();
+        const blogToUpdate = blogs[0];
+        const idToUpdate = blogToUpdate.id;
+
+        const updatedBlog = {
+            title: blogToUpdate.title,
+            author: blogToUpdate.author,
+            url: blogToUpdate.url,
+            likes: 500,
+            userId: creatorId
+        };
+
+        await api.put(`/api/blogs/${idToUpdate}`)
+            .send(updatedBlog)
+            .expect(200);
     });
 });
 
