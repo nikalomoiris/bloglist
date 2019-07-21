9c0a9cbdbe501fd76b8b92d762317c3cfdbf960d
nikalomiris <nikalomiris@gmail.com>
2019-07-21 10:00:34 -0400

    Part 4 bloglist expansion step 7 solution

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ const bodyParser = require('body-parser');
 const app = express();
 const cors = require('cors');
 const blogsRouter = require('./controllers/blogs');
+const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 const middleware = require('./utils/middleware');
 const mongoose = require('mongoose');
 const logger = require('./utils/logger');
-const usersRouter = require('./controllers/users');
 
 logger.info('Connecting to ', config.MONGODB_URI);
 
@@ -22,6 +23,7 @@ app.use(bodyParser.json());
 app.use(middleware.requestLogger);
 app.use('/api/blogs', blogsRouter);
 app.use('/api/users', usersRouter);
+app.use('/api/login', loginRouter);
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
