04e36e524368b5bd82cd4eb8249bc65544981be5
Nikolaos Kalomoiris <nikalomoiris@gmail.com>
2021-10-06 14:10:06 +0300

    added .env file and rest requests

---
+++ build/precache-manifest.469ac622e4a771a9060736d45e7b06d7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "231658dc0aba0fe51318731dea11c5c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9cdbf7bfd2576edf6411",
+    "url": "/static/js/2.68d775b2.chunk.js"
+  },
+  {
+    "revision": "0344cd318a5ce7e32a9b",
+    "url": "/static/js/main.39e94ef8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4fc49f9d8458b5b205ea5b2521ae24ac.js"
+  "/precache-manifest.469ac622e4a771a9060736d45e7b06d7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
